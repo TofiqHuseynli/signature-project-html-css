df4dfa5f096370bd5f299647e0e0152bc7d74194
Tofiq Huseynli <tofiq789@gmail.com>
2024-02-20 15:54:33 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let swiperSocial = new Swiper('.social-swiper', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    grabCursor: true,
+    centeredSlides: 'auto',
+    navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+   
+  });
+
